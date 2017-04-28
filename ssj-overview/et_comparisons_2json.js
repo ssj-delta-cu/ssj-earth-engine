c1b5e9f6fd7547fa5d4d1695e4be8301cfac95af
ucd.cws.ee.data <ucd.cws.ee.data@gmail.com>
2017-04-28 19:52:02 +0000

    Updated ssj-overview/et_comparisons_2json.js (added 7 lines, deleted 7 lines).

--- ssj-overview/et_comparisons_2json.js
+++ ssj-overview/et_comparisons_2json.js
@@ -6,23 +6,23 @@ var select_water_year = function(water_year){
       itrc_co:{image:ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-itrc-metric/itrc_et_wy2015_v2-1-0")},
       itrc_or:{image:ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-itrc-metric/itrc_et_wy2015_v2-0-1")},
       disalexi_or:{image:ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-disalexi/disalexi_et_wy2015_v2-0-0")},
-      disalexi_co:{image:ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-disalexi/disalexi_et_wy2016_v2-1-0")},
+      disalexi_co:{image:ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-disalexi/disalexi_et_wy2015_v2-1-0")},
       };
     
     var landIQ_wy2015 = ee.Image('users/ucd-cws-ee-data/ssj-delta-cu/ssj-landuse/landiq_2015_v2016-06-16');
     var landcover = landIQ_wy2015.select(['b2']);
   }
-  else if(water_year == 2015){
+  else if(water_year == 2016){
 
     var methods={
-      itrc_co:{image:ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-itrc-metric/itrc_et_wy2015_v2-1-0")},
-      itrc_or:{image:ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-itrc-metric/itrc_et_wy2015_v2-0-1")},
-      disalexi_or:{image:ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-disalexi/disalexi_et_wy2015_v2-0-0")},
+      itrc_co:{image:ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-itrc-metric/itrc_et_wy2016_v2-1-0")},
+      itrc_or:{image:ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-itrc-metric/itrc_et_wy2016_v2-0-1")},
+      disalexi_or:{image:ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-disalexi/disalexi_et_wy2016_v2-0-0")},
       disalexi_co:{image:ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-disalexi/disalexi_et_wy2016_v2-1-0")},
       };
     
-    var landIQ_wy2015 = ee.Image('users/ucd-cws-ee-data/ssj-delta-cu/ssj-landuse/landiq_2015_v2016-06-16');
-    var landcover = landIQ_wy2015.select(['b2']);
+    var landIQ_wy2016 = ee.Image('users/ucd-cws-ee-data/ssj-delta-cu/ssj-landuse/landiq_2016_v2017-05-25');
+    var landcover = landIQ_wy2016.select(['b1']);
   }
   
   return({methods: methods, landcover: landcover});
