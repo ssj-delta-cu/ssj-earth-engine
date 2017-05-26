e9d33dd3628127e55d4e0463c3217502ff08fd4d
ucd.cws.ee.data <ucd.cws.ee.data@gmail.com>
2017-05-26 18:57:15 +0000

    Updated ssj-overview/et_comparisons_2json.js (added 1 line, deleted 1 line).

--- ssj-overview/et_comparisons_2json.js
+++ ssj-overview/et_comparisons_2json.js
@@ -10,7 +10,7 @@ var select_water_year = function(water_year){
       sims:{image: ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-sims/sims_et_wy2015_v2-0-0")},
       eto:{image: ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-weather/eto_wy2015")},
       calsimetaw:{image: ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-calsimetaw/calsimetaw_et_wy2015_v2-0-0")},
-      detaw:{image: ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-detaw/detaw_et_wy2015_v2-0-0")},
+      detaw:{image: ee.Image("users/ucd-cws-ee-data/ssj-delta-cu/ssj-detaw/detaw_et_wy2015_v2-0-1")},
 
     };
     
